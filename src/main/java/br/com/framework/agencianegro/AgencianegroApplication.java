2f54973f955c810b6235c718ce9c68ab52c8b6c8
Kaue Lima <kauegouveia@gmail.com>
2024-03-12 13:06:37 -0300

    teste

---
+++ src/main/java/br/com/framework/agencianegro/AgencianegroApplication.java
@@ -0,0 +1,13 @@
+package br.com.framework.agencianegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencianegroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencianegroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/framework/agencianegro/AgencianegroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.framework.agencianegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencianegroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
